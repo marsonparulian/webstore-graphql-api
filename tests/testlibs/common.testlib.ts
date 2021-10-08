d1213f37253ee683dcf75b64c67327686a1d1793
marson parulian <marson.parulian@gmail.com>
2021-10-08 12:25:47 +1100

    init IProduct interface and implementation

---
+++ tests/testlibs/common.testlib.ts
@@ -0,0 +1,7 @@
+// This file contains commond data test
+
+import { IProduct } from "../../src/types/common";
+
+export const product1: IProduct = {
+    name: "Ultra Milk Strawberry", description: "200 mL 3 packs."
+}

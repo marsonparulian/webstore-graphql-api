115f6394123b6dc920b3573bab4e661563379bfc
marson parulian <marson.parulian@gmail.com>
2021-10-10 10:24:22 +1100

    refactor & add 'product' test data

--- tests/testlibs/products.testlib.ts
+++ tests/testlibs/products.testlib.ts
@@ -1,5 +1,4 @@
-// This file contains commond data test
-
+// This file contains `product` data test
 import { IProduct } from "../../src/types/common";
 
 export const product1: IProduct = {
@@ -7,3 +6,5 @@ export const product1: IProduct = {
 };
 export const product2 = { name: "Tamar Valley Kids Vanilla", description: "Tamar Valley yoghurt for kids 400mL vanilla flavour." };
 export const product3: IProduct = { name: "Strawberry regular punet", description: "Strawberry 600 gr" };
+export const product4: IProduct = { name: "Fresh Milk Regular", description: "Fresh cow milk  2 Litres" };
+export const product5: IProduct = { name: "Fresh Light Milk Regular", description: "Fresh cow light milk 2 Litres." };

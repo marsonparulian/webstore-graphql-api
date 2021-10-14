9e20ba39db6e430d71d4741ea357e201d315f322
marson parulian <marson.parulian@gmail.com>
2021-10-14 22:02:33 +1100

    add more 'product' test data

--- tests/testlibs/products.testlib.ts
+++ tests/testlibs/products.testlib.ts
@@ -8,3 +8,8 @@ export const product2 = { name: "Tamar Valley Kids Vanilla", description: "Tamar
 export const product3: IProduct = { name: "Strawberry regular punet", description: "Strawberry 600 gr" };
 export const product4: IProduct = { name: "Fresh Milk Regular", description: "Fresh cow milk  2 Litres" };
 export const product5: IProduct = { name: "Fresh Light Milk Regular", description: "Fresh cow light milk 2 Litres." };
+export const product6: IProduct = { name: "Tak Hing Custard", description: "Tak Hing custard bun 6 pieces" };
+export const product7: IProduct = { name: "Tak Hing BBQ Chicken", description: "Tak Hing BBQ chicken bun 4 pieces." };
+export const product8: IProduct = { name: "Devondale Vitamin Milk Vanilla Thrilla", description: "Devondale Vital Kids.Vitamin milk.Flavour: Vanilla Thrilla " };
+export const product9: IProduct = { name: "Devondale Vitamin Milk Berry Buff", description: "Devondale Vital Kids. Vitamin milk for kids. Flavour: Berry Buff" };
+

ae1d6c0f217dff643647f47c48c7cea1453af84f
marson parulian <marson.parulian@gmail.com>
2021-10-16 14:20:56 +1100

    append user data-source to server

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import typeDefs from "./schema.graphql";
 import resolvers from "./resolver.graphql"
 import ProductModel from "./models/product.model";
 import Product from "./data-sources/products";
+import UserModel from "./models/user.model";
+import Users from "./data-sources/users";
 import "dotenv/config";
 
 // Create server
@@ -12,6 +14,7 @@ const server = new ApolloServer({
     resolvers,
     dataSources: () => ({
         products: new Product(ProductModel),
+        users: new Users(UserModel),
     }),
     plugins: [
         ApolloServerPluginLandingPageGraphQLPlayground,

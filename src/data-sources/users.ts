270adfbd2804d35012b354ee5d1513552d849053
marson parulian <marson.parulian@gmail.com>
2021-10-19 12:01:28 +1100

    handle login process

--- src/data-sources/users.ts
+++ src/data-sources/users.ts
@@ -1,6 +1,6 @@
 import { MongoDataSource } from "apollo-datasource-mongodb";
 import bcrypt from "bcrypt";
-import { RegisterInput, UserDocument } from "../types/auth";
+import { RegisterInput, LoginInput, UserDocument, UserSession } from "../types/auth";
 import { IContext } from "../types/common";
 
 class Users extends MongoDataSource<UserDocument, IContext>{
@@ -15,6 +15,30 @@ class Users extends MongoDataSource<UserDocument, IContext>{
         await user.save();
 
         return user;
+    }
+    /**
+     * User login
+     */
+    async login(loginInput: LoginInput): Promise<UserSession> {
+        // Fetch user from DB
+        const user = await this.model.findOne({ email: loginInput.email }).lean();
+
+        // Does user exist ?
+        if (!user) throw new Error(`User with email ${loginInput.email} is not found`);
+
+        // verify the password is a match
+        const isPasswordAMatch = bcrypt.compare(loginInput.password, user.password);
+        if (!isPasswordAMatch) throw new Error("Incorrect password");
+
+        // TODO Generate jwt token
+        const token = "TEMP TOKEN";
+
+        // Return
+        return {
+            name: user.name,
+            email: user.email,
+            token,
+        };
     }
 }
 

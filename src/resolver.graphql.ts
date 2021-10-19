58b0f4cb1295447074b1fb2e148333f7cabe8057
marson parulian <marson.parulian@gmail.com>
2021-10-19 12:02:32 +1100

    'login' resolver

--- src/resolver.graphql.ts
+++ src/resolver.graphql.ts
@@ -1,18 +1,20 @@
-import { RegisterInput } from "./types/auth";
+import { RegisterInput, LoginInput } from "./types/auth";
 import { IContext, IProductParams } from "./types/common";
 
 // Graphql resolver 
 const resolver = {
     Query: {
         paginatedProducts: async (_: never, params: IProductParams, context: IContext) => {
-            // return context.dataSources.products.getProducts(params);
             return context.dataSources.products.getPaginatedProducts(params);
         },
     },
     Mutation: {
         register: async (_: never, args: { registerInput: RegisterInput }, context: IContext) => {
             return context.dataSources.users.register(args.registerInput);
-        }
+        },
+        login: async (_: never, { loginInput }: { loginInput: LoginInput }, context: IContext) => {
+            return context.dataSources.users.login(loginInput)
+        },
     }
 };
 export default resolver;

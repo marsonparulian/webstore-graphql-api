7398e5d6e04170c2bbb38f7db1647b49a7190a44
marson parulian <marson.parulian@gmail.com>
2021-10-19 12:03:40 +1100

    add helper to reduce / convert type

--- src/hellpers/auth.helper.ts
+++ src/hellpers/auth.helper.ts
@@ -1,5 +1,5 @@
 // This file contains helper related to authentication / authorization
-import { RegisterInput, IUser } from "../types/auth";
+import { RegisterInput, IUser, LoginInput } from "../types/auth";
 
 /**
  * Reduce `RegisterInput` to `IUser`
@@ -9,4 +9,13 @@ export const reduceRegisterInputToUser = (registerInput: RegisterInput): IUser =
     const { password, ...user } = registerInput;
 
     return user;
+}
+/**
+ * Reduce Data used inregistration to data used in login
+ */
+export const reduceRegisterInputToLoginInput = (registerInput: RegisterInput): LoginInput => {
+    return {
+        email: registerInput.email,
+        password: registerInput.password,
+    };
 }

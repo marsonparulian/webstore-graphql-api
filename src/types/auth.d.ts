aaf09cabfd82c83cf32e9d96b59d9bcc81a2a39b
marson parulian <marson.parulian@gmail.com>
2021-10-20 14:02:04 +1100

    refactor auth related types

--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -1,19 +1,20 @@
 // This file contain authentication-related type definitions
 import { Document } from "mongoose";
 
-/** Data submitted when user register */
-export interface RegisterInput {
-    name: string;
-    email: string;
-    password: string;
-}
-/** Representing a user, excluding 'private' info e.g.: password */
+/** Public compact version of a user */
 export interface IUser {
     name: string;
     email: string;
 }
+/** User secret used in registration */
+interface RegisterSecret {
+    password: string;
+}
+/** Data submitted when user register */
+export type RegisterInput = IUser & RegisterSecret;
+
 /** User document saved in DB */
-export type UserDocument = IUser & Document & { password: string };
+export type UserDocument = RegisterInput & Document;
 
 /** login form input */
 export interface LoginInput {

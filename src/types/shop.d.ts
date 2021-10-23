13a6c669256f4b7bc06b8f4bafd795ed4eb9bc97
marson parulian <marson.parulian@gmail.com>
2021-10-23 12:48:11 +1100

    ts shop-related type def

---
+++ src/types/shop.d.ts
@@ -0,0 +1,15 @@
+// This file contains type declaration related to shopping
+import { Document } from "mongoose";
+import { Product } from "./common";
+import { IUser } from "./auth"
+
+export interface CartItem {
+    product: string | Product;
+    qty: number;
+}
+export interface Cart {
+    _id: string;
+    user: string | IUser;
+    cartItems: CartItem[];
+}
+export type CartDocument = Cart & Document;
